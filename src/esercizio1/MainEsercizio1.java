c6ab2b37b7cdb21baffcdf872de7f80d959986db
lucawar <lucaguerra92@yahoo.it>
2023-06-27 19:06:52 +0200

    esercizioGiorno3

--- src/esercizio1/MainEsercizio1.java
+++ src/esercizio1/MainEsercizio1.java
@@ -5,6 +5,8 @@ public class MainEsercizio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("daje");
+		System.out.println("prova");
+
 	}
 
 }
