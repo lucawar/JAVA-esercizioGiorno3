526ce0d908085cb8b930b5471bb0e93e6fe05d56
lucawar <lucaguerra92@yahoo.it>
2023-06-28 14:57:37 +0200

    esercizioGiorno3-commit3

--- src/esercizio1/MainEsercizio1.java
+++ src/esercizio1/MainEsercizio1.java
@@ -5,15 +5,27 @@ public class MainEsercizio1 {
 	public static void main(String[] args) {
 //VARIABILI STRINGA
 		String input = "FORZA ROMA!";
+		String input2 = "FORZAROMA!";
+
 		boolean lengthStr = StringaPariDispari(input);
+		boolean lengthStr2 = StringaPariDispari(input2);
+
 		System.out.println("La stringa ha una lunghezza pari?");
 		System.out.println(lengthStr);
+		System.out.println("La stringa ha una lunghezza pari?");
+		System.out.println(lengthStr2);
 
 //VARIABILI ANNO
 		int anno = 1927;
+		int anno2 = 2020;
+
 		boolean annoBis = annoBisestile(anno);
-		System.out.println("L'anno " + anno + " è bisestile? " + annoBis);
+		boolean annoBis2 = annoBisestile(anno2);
+
+		System.out.println("L'anno " + anno + " è bisestile? ");
 		System.out.println(annoBis);
+		System.out.println("L'anno " + anno2 + " è bisestile? ");
+		System.out.println(annoBis2);
 	}
 
 //METODO PER VERIFICARE SE LA STRINGA INSERITA è PARA O DISPARA
@@ -29,12 +41,10 @@ public class MainEsercizio1 {
 
 //METODO PER VERIFICARE SE UN ANNO è BISESTILE
 	public static boolean annoBisestile(int anno) {
-		if (anno % 400 == 0) {
+		if (anno % 400 == 0 || anno % 4 == 0) {
 			return true;
 		} else if (anno % 100 == 0) {
 			return false;
-		} else if (anno % 4 == 0) {
-			return true;
 		} else {
 			return false;
 		}
--- src/esercizio3/MainEsercizio3.java
+++ src/esercizio3/MainEsercizio3.java
@@ -1,9 +1,38 @@
 package esercizio3;
 
+import java.util.Scanner;
+
 public class MainEsercizio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		String inputString;
+
+//CICLO WHILE CHE CONSENTE DI INSERIRE STRINGHE
+		while (true) {
+			System.out.println("Per terminare il programma,inserire :q");
+			System.out.println("Inserisci una parola:");
+			inputString = input.nextLine();
+
+//CONDIZIONE CHE TERMINA IL PROGRAMMA QUANDO VIENE INSERITA LA STRINGA ":q"
+			if (inputString.equals(":q")) {
+				System.out.println("Programma terminato.");
+				break;
+			}
+//METODO "StringBuilder" + "append" PER SUDDIVIDERE LE PAROLE CON UNA ","
+			StringBuilder output = new StringBuilder();
+			for (int i = 0; i < inputString.length(); i++) {
+				output.append(inputString.charAt(i));
+				if (i != inputString.length() - 1) {
+					output.append(",");
+				}
+			}
+
+			System.out.println("Stringa suddivisa: " + output.toString());
+		}
+
+		input.close();
 
 	}
 
