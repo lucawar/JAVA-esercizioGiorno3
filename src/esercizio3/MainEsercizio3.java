64825c512e558f561fa76183d69c6bc94164d9b9
lucawar <lucaguerra92@yahoo.it>
2023-06-28 13:02:59 +0200

    esercizioGiorno3

---
+++ src/esercizio3/MainEsercizio3.java
@@ -0,0 +1,10 @@
+package esercizio3;
+
+public class MainEsercizio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/esercizio4/MainEsercizio4.java
@@ -0,0 +1,10 @@
+package esercizio4;
+
+public class MainEsercizio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
